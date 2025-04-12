a2c9d7246dde0fc777a4bd495a4df38f83b6fc5f
Kobe Amoh <kobexamoh@gmail.com>
2025-04-12 04:23:23 -0600

    Update App.jsx for hard-coding test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Landing from './views/Landing';
 import IntroCopy from './views/IntroCopy';
+import Card from './components/Card'
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
       <Landing />
       <IntroCopy />
+
+      <Card
+        name="Julia Hayter"
+        party="NDP"
+        region="Southern Alberta"
+        division="Calgary-Edgemont"
+        photo="src/assets/headshots/member-6.png"
+      />
     </div>
   );
 }
