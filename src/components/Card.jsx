701ebb02c4d703ba7308c5a8c1aee21de36e52ca
Kobe Amoh <kobexamoh@gmail.com>
2025-04-13 21:13:11 -0600

    Update width of card images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ name, party, region, division, photo }) {
     return (
         <div className="p-4 border rounded-lg shadow bg-white text-gray-900">
-            <img src={photo} alt={`${name} headshot`} className="w-full h-48 object-cover rounded mb-4" />
+            <img src={photo} alt={`${name} headshot`} className="w-75 h-48 object-cover rounded mb-4" />
             <h3 className="text-xl font-bold">{name}</h3>
             <p className="text-sm text-gray-700">{party}</p>
             <p className="text-sm">{division}, {region}</p>
