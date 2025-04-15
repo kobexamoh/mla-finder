c8aef5d07f64bc6ccc6d38253465c940b1920602
Kobe Amoh <kobexamoh@gmail.com>
2025-04-15 14:14:11 -0600

    Begin implementing search

--- src/views/SearchBar.jsx
+++ src/views/SearchBar.jsx
@@ -1,6 +1,10 @@
 export default function SearchBar() {
+    function search(formData) {
+        const query = formData.get("search");
+        alert(`You're looking for '${query}'`);
+    }
     return (
-        <form>
+        <form action={search}>
             <input name="search" />
             <button type="submit">Search</button>
         </form>
