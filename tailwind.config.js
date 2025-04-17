4a9782e7a7fc24ad418776b9d22b95db0a1abd19
Kobe Amoh <kobexamoh@gmail.com>
2025-04-17 07:59:39 -0600

    Add container reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
+    },
     extend: {},
   },
   plugins: [],
