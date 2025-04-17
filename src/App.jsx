1f242efcbf74515e25c55ee33c87fa147a35e554
Kobe Amoh <kobexamoh@gmail.com>
2025-04-17 07:33:11 -0600

    Place content in container

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,25 @@ import mlas from './data/mlas.json';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900">
-      <Landing />
-      <IntroCopy />
+    <div className="container">
+      <div className="min-h-screen bg-gray-100 text-gray-900">
+        <Landing />
+        <IntroCopy />
 
-      <SearchBar/>
+        <SearchBar />
 
-      <div className="card-grid">
-        {mlas.map( mla => (
-          <Card
-            key={mla.id}
-            name={mla.name}
-            party={mla.party}
-            region={mla.region}
-            division={mla.division}
-            photo={mla.photo}
-          />
-        ))}
+        <div className="card-grid">
+          {mlas.map((mla) => (
+            <Card
+              key={mla.id}
+              name={mla.name}
+              party={mla.party}
+              region={mla.region}
+              division={mla.division}
+              photo={mla.photo}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
