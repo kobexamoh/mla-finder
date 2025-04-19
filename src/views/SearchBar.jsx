4bf492fdf7c8d8adb564c6d6784bcbbcfacb6a1f
Kobe Amoh <kobexamoh@gmail.com>
2025-04-19 11:05:44 -0600

    Update searchbar component styling

--- src/views/SearchBar.jsx
+++ src/views/SearchBar.jsx
@@ -4,9 +4,16 @@ export default function SearchBar() {
         alert(`You're looking for '${query}'`);
     }
     return (
+      <div class="mt-4">
         <form action={search}>
-            <input name="search" />
-            <button type="submit">Search</button>
+          <input
+            name="search"
+            className="border-2 border-gray-300 px-4 focus:border-sky-500 rounded-lg py-2 w-10/12"
+            placeholder="Enter a name or party..."
+          />
+          <button type="submit" className="w-2/12">Search</button> 
+          {/* TODO: replace with search icon */}
         </form>
+      </div>
     );
 }
